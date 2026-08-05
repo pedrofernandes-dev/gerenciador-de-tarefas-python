1225795b855c085f17a91bd7dbebbef0409a7500
pedrofernandes-dev <acostafernandesp@gmail.com>
2026-08-04 22:29:32 -0300

    refactor: organiza código utilizando funções

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 opcao = ""
 tarefas = []
 
+def mostrar_menu():
+    print("============================")
+    print("GERENCIADOR DE TAREFAS")
+    print("============================")
+    print()
+    print("1 - Adicionar tarefa")
+    print("2 - Listar tarefas")
+    print("3 - Sair")
+
+def adicionar_tarefa():
+    tarefa = input("Digite a tarefa a ser adicionada: ")
+    tarefas.append(tarefa)
+    print("Tarefa adicionada com sucesso!")
+
+def listar_tarefas():
+    if len(tarefas) == 0:
+        print("Nenhuma tarefa cadastrada.")
+    else:
+        for tarefa in tarefas:
+            print(tarefa)
+
 while opcao != "3":
 
     print("============================")
@@ -15,15 +36,9 @@ while opcao != "3":
     print("Você escolheu a opção: " + opcao)
 
     if opcao == "1":
-        tarefa = input("Digite a tarefa a ser adicionada: ")
-        tarefas.append(tarefa)
-        print("Tarefa adicionada com sucesso!")
+        adicionar_tarefa()
     elif opcao == "2":
-        if len(tarefas) == 0:
-            print("Nenhuma tarefa cadastrada.")
-        else:
-            for tarefa in tarefas:
-                print(tarefa)
+        listar_tarefas()
     elif opcao == "3":
         print("Encerrando o programa. Até logo!")
     else:
