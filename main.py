99b7237facd493a49ed8c40c027184f5e12481f2
pedrofernandes-dev <acostafernandesp@gmail.com>
2026-08-07 11:41:16 -0300

    feat: adiona edição de tarefas e organiza código com funções

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def mostrar_menu():
     print()
     print("1 - Adicionar tarefa")
     print("2 - Listar tarefas")
-    print("3 - Sair")
+    print("3 - Editar tarefa")
+    print("4 - Sair")
 
 def adicionar_tarefa():
     tarefa = input("Digite a tarefa a ser adicionada: ")
@@ -22,15 +23,18 @@ def listar_tarefas():
         for tarefa in tarefas:
             print(tarefa)
 
-while opcao != "3":
+def editar_tarefas():
+    if len(tarefas) == 0:
+        print("Nenhuma tarefa cadastrada.")
+    else:
+        for tarefa in tarefas:
+            print(tarefa)
+    indice = int(input("Digite o número da tarefa que deseja editar: "))
+    novo_nome = input("Digite o novo nome da tarefa: ")
+    tarefas[indice-1] = novo_nome    
 
-    print("============================")
-    print("GERENCIADOR DE TAREFAS")
-    print("============================")
-    print()
-    print("1 - Adicionar tarefa")
-    print("2 - Listar tarefas")
-    print("3 - Sair")
+while opcao != "4":
+    mostrar_menu()
 
     opcao = input("Escolha uma opção: ")
     print("Você escolheu a opção: " + opcao)
@@ -40,6 +44,8 @@ while opcao != "3":
     elif opcao == "2":
         listar_tarefas()
     elif opcao == "3":
+        editar_tarefas()
+    elif opcao == "4":
         print("Encerrando o programa. Até logo!")
     else:
         print("Opção inválida. Por favor, escolha uma opção válida.")
